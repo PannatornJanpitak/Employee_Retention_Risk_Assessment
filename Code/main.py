6fa79410b06031dd6addb815a1cf3e6000bb7082
anbun_ <166464529+PannatornJanpitak@users.noreply.github.com>
2024-04-09 09:03:33 +0700

    Add main file
    to run UI app

---
+++ Code/main.py
@@ -0,0 +1,14 @@
+"""
+
+This file show training progress before getting the model.
+
+"""
+from UI_classify_employee import EmployeeRetentionApp
+
+def main():
+   app = EmployeeRetentionApp()
+
+if __name__ == "__main__":
+   main()
+
+
